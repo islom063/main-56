e999b54b2c340ee4de4bb1df84157661b675201b
islombaxtiyorov063@gmail.com <islombaxtiyorov063@gmail.com>
2026-09-16 23:18:06 +0500

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-class talaba:
-    def __init__(self, ism, fm):
-        self.ism = ism
-        self.fm = fm
-    def chiqar(self ):
-        return f"{self.ism} {self.fm}"
+# class talaba:
+#     def __init__(self, ism, fm):
+#         self.ism = ism
+#         self.fm = fm
+#     def chiqar(self ):
+#         return f"{self.ism} {self.fm}"
+#
+#     def faqat_ism(self):
+#         return self.ism
+#
+# a = talaba('islom', 'baxtiyorov')
+# print(a.chiqar())
 
-a = talaba('islom', 'baxtiyorov')
-print(a.chiqar())
+
+
+class Chiqar:
+    def __init__(self, ism, familiya):
+        self.ism = ism
+        self.familiya = familiya
+
+    def Tekshir(self):
+        return f"{self.ism} {self.familiya}"
+
+    def faqat_ism(self):
+        return self.ism
+
+a = Chiqar(ism='ALi', familiya="Jabborov")
+print(a.faqat_ism())
